73066c805f206cee44678347ef8f8cffff2fa25d
Dmitry Protasevich <70170204+ArchiProgrammer22@users.noreply.github.com>
2022-11-04 10:46:41 +0200

    LN 006 Optimizing fullscreen flags

--- app/src/main/java/com/example/webviewapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/webviewapp/presentation/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.webviewapp.presentation
 
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.webviewapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,21 +11,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        changeOnFullscreen()
-    }
-
-    private fun changeOnFullscreen() {
-        supportActionBar?.hide()
-
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
     }
 }
